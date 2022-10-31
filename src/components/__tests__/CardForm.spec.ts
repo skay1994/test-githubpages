f0514e50e330161ce8b6cdb0c51a8f24fe6515eb
Jorge Carlos <skay_1994@yahoo.com.br>
2022-10-30 21:56:50 -0300

    Updated CardForm.spec.ts tests

--- src/components/__tests__/CardForm.spec.ts
+++ src/components/__tests__/CardForm.spec.ts
@@ -19,4 +19,49 @@ describe("CardForm Tests", function () {
     expect(wrapper.find('[for="security-code"]').text()).toContain("CVC");
     expect(wrapper.find("#add-card").text()).toContain("ADICIONAR CARTÃO");
   });
+
+  it("Set card number value", async () => {
+    const wrapper = mount(CardForm);
+
+    await wrapper.find('[id="card-number"]').setValue("4789878978979878");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().cardNumber).toBeTruthy();
+    // @ts-ignore
+    expect(wrapper.emitted().cardNumber[0][0]).toContain("4789 8789 7897 9878");
+  });
+
+  it("Set card holder value", async () => {
+    const wrapper = mount(CardForm);
+    const cardHolder = "Usuario Teste";
+
+    await wrapper.find('[id="card-holder"]').setValue(cardHolder);
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().cardHolder).toBeTruthy();
+    // @ts-ignore
+    expect(wrapper.emitted().cardHolder[0][0]).toContain(cardHolder);
+  });
+
+  it("Set expiration date value", async () => {
+    const wrapper = mount(CardForm);
+
+    await wrapper.find('[id="expiration-date"]').setValue("1225");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().expirationDate).toBeTruthy();
+    // @ts-ignore
+    expect(wrapper.emitted().expirationDate[0][0]).toContain("12/25");
+  });
+
+  it("Set security code value", async () => {
+    const wrapper = mount(CardForm);
+
+    await wrapper.find('[id="security-code"]').setValue("12255");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().securityCode).toBeTruthy();
+    // @ts-ignore
+    expect(wrapper.emitted().securityCode[0][0]).toContain("1225");
+  });
 });
