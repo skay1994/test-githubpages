ddd94107b9fbac35caa70f5c1b71fc81b41482e9
Jorge Carlos <skay_1994@yahoo.com.br>
2022-10-31 14:26:08 -0300

    Updated Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -22,14 +22,14 @@
         <g mask="url(#mask0_3_2547)">
           <g filter="url(#filter0_f_3_2547)">
             <path
+              :fill="firstColor"
               d="M451.518 -135.506C473.881 -89.3531 414.166 -13.4917 318.142 33.9349C222.118 81.3615 126.147 82.3939 103.784 36.2409C81.4215 -9.91221 141.136 -85.7735 237.16 -133.2C333.184 -180.627 429.156 -181.659 451.518 -135.506Z"
-              fill="black"
             />
           </g>
           <g filter="url(#filter1_f_3_2547)">
             <path
+              :fill="secondaryColor"
               d="M399.134 -169.756C421.497 -123.603 361.783 -47.742 265.758 -0.315356C169.734 47.1113 73.7629 48.1437 51.4003 1.99062C29.0377 -44.1624 88.7521 -120.024 184.776 -167.45C280.8 -214.877 376.772 -215.909 399.134 -169.756Z"
-              fill="gray"
             />
           </g>
         </g>
@@ -119,6 +119,11 @@
 </template>
 <script lang="ts">
 const types = ["default", "visa", "mastercard"];
+export const colors = {
+  visa: ["#436D99", "#2D57F2"],
+  mastercard: ["#DF6F29", "#C69347"],
+  default: ["black", "gray"],
+};
 
 export default {
   props: {
@@ -142,6 +147,18 @@ export default {
     securityCode: {
       type: String,
       default: "123",
+    },
+  },
+  computed: {
+    firstColor(): string {
+      if (types.includes(this.type)) return colors[this.type][0];
+
+      return colors.default[0];
+    },
+    secondaryColor(): string {
+      if (types.includes(this.type)) return colors[this.type][1];
+
+      return colors.default[1];
     },
   },
 };
