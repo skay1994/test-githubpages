e9bca288d9fcb8b81bcac252738ac2f7ed155ee4
Jorge Carlos <skay_1994@yahoo.com.br>
2022-10-30 21:55:46 -0300

    Updated Card.spec.ts tests

--- src/components/__tests__/Card.spec.ts
+++ src/components/__tests__/Card.spec.ts
@@ -18,4 +18,97 @@ describe("Card Component Tests", function () {
       "src/assets/svgs/cc-chip.svg"
     );
   });
+
+  it("Change card number value", () => {
+    const cardNumber = "4568 7894 1234 7894";
+    const wrapper = mount(Card, {
+      props: { cardNumber },
+    } as any);
+    const ccNumber = wrapper.find(".cc-info .cc-number");
+
+    expect(ccNumber.text()).toContain(cardNumber);
+  });
+
+  it("Change card holder value", () => {
+    const cardHolder = "Nome Teste";
+    const wrapper = mount(Card, {
+      props: { cardHolder },
+    } as any);
+    const ccHolder = wrapper.find(".cc-holder .value");
+
+    expect(ccHolder.text()).toContain(cardHolder);
+  });
+
+  it("Change expiration date value", () => {
+    const expirationDate = "11/25";
+    const wrapper = mount(Card, {
+      props: { expirationDate },
+    } as any);
+    const ccExpirationDate = wrapper.find(".cc-extra .value");
+
+    expect(ccExpirationDate.text()).toContain(expirationDate);
+  });
+
+  it("Change security code value", () => {
+    const securityCode = "11/25";
+    const wrapper = mount(Card, {
+      props: { securityCode },
+    } as any);
+    const ccSecurityCode = wrapper.find(".cc-security .value");
+
+    expect(ccSecurityCode.text()).toContain(securityCode);
+  });
+
+  it("Change card type as default", () => {
+    const type = "default";
+    const wrapper = mount(Card, {
+      props: { type },
+    } as any);
+    const logo = wrapper.find(".cc-logo span:nth-child(2) img");
+
+    expect(logo.attributes()["src"]).toContain(
+      "src/assets/svgs/cc-default.svg"
+    );
+  });
+
+  it("Change card type as visa", () => {
+    const type = "visa";
+    const wrapper = mount(Card, {
+      props: { type },
+    } as any);
+    const logo = wrapper.find(".cc-logo span:nth-child(2) img");
+
+    expect(logo.attributes()["src"]).toContain("src/assets/svgs/cc-visa.svg");
+  });
+
+  it("Change card type as mastercard", () => {
+    const type = "mastercard";
+    const wrapper = mount(Card, {
+      props: { type },
+    } as any);
+    const logo = wrapper.find(".cc-logo span:nth-child(2) img");
+
+    expect(logo.attributes()["src"]).toContain(
+      "src/assets/svgs/cc-mastercard.svg"
+    );
+  });
+
+  it("Change card type with invalid type", () => {
+    mount(Card, {
+      props: { type: "invalid" },
+      global: {
+        config: {
+          warnHandler(msg: string) {
+            expect(
+              msg,
+              '[Vue warn]: Invalid prop: custom validator check failed for prop "type"'
+            );
+          },
+        },
+      },
+    } as any);
+
+    const validator = Card.props.type.validator;
+    expect(validator("invalid")).toBeFalsy();
+  });
 });
